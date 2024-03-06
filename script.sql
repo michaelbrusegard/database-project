b1e5e904d191f3d3c03ca5b7b702c2279bdb8f7f
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-03-06 11:21:12 +0100

    add indexes and multiple dates per play

--- script.sql
+++ script.sql
@@ -32,9 +32,15 @@ CREATE TABLE seats (
 CREATE TABLE plays (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(255) NOT NULL,
-    date DATE NOT NULL,
     hall_id INTEGER NOT NULL,
     FOREIGN KEY (hall_id) REFERENCES halls(id)
+);
+
+CREATE TABLE showings (
+    play_id INTEGER NOT NULL,
+    date DATE NOT NULL,
+    PRIMARY KEY (play_id, date),
+    FOREIGN KEY (play_id) REFERENCES plays(id)
 );
 
 CREATE TABLE customers (
@@ -46,10 +52,10 @@ CREATE TABLE customers (
 
 CREATE TABLE tickets (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    play_id INTEGER NOT NULL,
+    play_date_id INTEGER NOT NULL,
     customer_id INTEGER NOT NULL,
     seat_id INTEGER NOT NULL,
-    FOREIGN KEY (play_id) REFERENCES plays(id),
+    FOREIGN KEY (play_date_id) REFERENCES showings(play_id, date),
     FOREIGN KEY (customer_id) REFERENCES customers(id),
     FOREIGN KEY (seat_id) REFERENCES seats(chair_number)
 );
@@ -121,3 +127,15 @@ CREATE TABLE tasks (
     FOREIGN KEY (play_id) REFERENCES plays(id),
     FOREIGN KEY (employee_id) REFERENCES employees(id)
 );
+
+CREATE INDEX idx_theatre_id ON halls(theatre_id);
+
+CREATE INDEX idx_hall_id ON areas(hall_id);
+
+CREATE INDEX idx_area_id ON seats(area_id);
+
+CREATE INDEX idx_hall_id_play_id ON plays(hall_id);
+
+CREATE INDEX idx_play_id_customer_id ON tickets(play_id, customer_id);
+
+CREATE INDEX idx_play_id_employee_id ON tasks(play_id, employee_id);

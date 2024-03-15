263627261c33a6ef3392fb2317849f79070ca948
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-03-15 10:56:30 +0100

    read lines in reverse for hovedscenen

--- seed.py
+++ seed.py
@@ -77,12 +77,12 @@ with open('files needed/hovedscenen.txt', 'r') as f:
 # Initialize variables
 current_area = None
 seat_number = 0
-row_number = len(lines) - 3
+row_number = 0
 
 # Iterate over each line in the file
-for i, line in enumerate(lines):
+for i, line in reversed(list(enumerate(lines))):
     # Skip the first line
-    if i == 0:
+    if i == len(lines) - 1:
         continue
 
     line = line.strip()  # Remove trailing newline
@@ -94,7 +94,7 @@ for i, line in enumerate(lines):
         current_area = cursor.lastrowid
     else:
         # Iterate over each character and create a seat only if the character is '0' or '1'
-        row_number -= 1
+        row_number += 1
         for j, char in enumerate(line):
             seat_number += 1
             if char == '0':
